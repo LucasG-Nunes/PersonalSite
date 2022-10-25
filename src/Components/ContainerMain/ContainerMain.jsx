f3982457dd2f6d6c61672fbca092ba2cc8c2c24d
LucasG-Nunes <lucasgnam12@gmail.com>
2022-10-25 14:00:58 -0300

    :fire: Build

--- src/Components/ContainerMain/ContainerMain.jsx
+++ src/Components/ContainerMain/ContainerMain.jsx
@@ -12,10 +12,10 @@ const ContainerMain = () => {
                     </Link>
                 </article>
                 <article className="teste">
-                    <a className="a" href="https://github.com/LucasG-Nunes"><i class="bi bi-github"></i></a>
-                    <a href="https://www.linkedin.com/in/lucas-gnunes/"><i class="bi bi-linkedin"></i></a>
-                    <a href="https://api.whatsapp.com/send?phone=5581994532077&text=Ol%C3%A1%2C%20Vim%20pelo%20seu%20site!"><i class="bi bi-whatsapp"></i></a>
-                </article   >
+                    <a className="a" href="https://github.com/LucasG-Nunes" target="blank"><i class="bi bi-github"></i></a>
+                    <a href ="https://www.linkedin.com/in/lucas-gnunes/" target="blank"><i class="bi bi-linkedin"></i></a>
+                    <a href="https://api.whatsapp.com/send?phone=5581994532077&text=Ol%C3%A1%2C%20Vim%20pelo%20seu%20site!" target="blank"><i class="bi bi-whatsapp"></i></a>
+                </article>
             </container>
         </>
     )
